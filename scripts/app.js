9e04c2fbfa426b6b479146c332d981056b63b5a4
Danijel Stanic <danijel.stanich@gmail.com>
2021-01-15 23:46:21 +0100

    Added createGrid function

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+function createGrid(x) {
+    container.style['grid-template-rows'] = 'repeat('+x+', 1fr)';
+    container.style['grid-template-columns'] = 'repeat('+x+', 1fr)';
+
+    for (let i = 0; i < (x*x); i++) {
+        let cell = document.createElement("div");
+        cell.classList.add('cell')
+        container.appendChild(cell)
+    }
+}
+
+let container = document.querySelector('#container');
+
+createGrid(16);
