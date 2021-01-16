a247676a250185072634f93a845b78078ae31d4b
Danijel Stanic <danijel.stanich@gmail.com>
2021-01-16 19:24:01 +0100

    Added an option to custimize grid size

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,12 @@
 function createGrid(x) {
+    container.innerHTML = '';
     container.style['grid-template-rows'] = 'repeat('+x+', 1fr)';
     container.style['grid-template-columns'] = 'repeat('+x+', 1fr)';
 
     for (let i = 0; i < (x*x); i++) {
         let cell = document.createElement("div");
         cell.classList.add('cell')
+        cell.removeAttribute('style');
         container.appendChild(cell)
         cell.addEventListener('mouseenter', paintCell);
     }
@@ -14,6 +16,23 @@ function paintCell(e) {
     e.target.style["background-color"] = "black";
 }
 
+function getSize() {
+    let gridSize = document.querySelector('#grid-input').value;
+    if (Number.isInteger(Number(gridSize))) {
+        if (Number(gridSize) <= 100) {
+            createGrid(gridSize);
+        } else {
+            alert('Please enter a number smaller or equal to 100')
+        }   
+    } else {
+        alert('Please enter an integer')
+    }
+    
+}
+
 let container = document.querySelector('#container');
 
 createGrid(16);
+
+let generate = document.querySelector('#grid-submit')
+generate.addEventListener('click', getSize);
